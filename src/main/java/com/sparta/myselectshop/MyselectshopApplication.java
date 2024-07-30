42051beb6e1f076971fce4522aae8b5dff0077e3
lsw71311 <lsw71311@naver.com>
2024-07-30 14:09:10 +0900

    Feat: 스케줄링 기능(매일 01시에 관심상품 가격 업데이트)

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -3,7 +3,9 @@ package com.sparta.myselectshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling   //메서드를 특정 간격 또는 시간에 실행되도록 스케줄링 활성화
 @EnableJpaAuditing //엔티티 객체가 생성이 되거나 변경이 되었을 때 자동으로 값을 등록
 @SpringBootApplication
 public class MyselectshopApplication {
