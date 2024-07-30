4505e564744094bd799106411ab71f737a45e7c6
lsw71311 <lsw71311@naver.com>
2024-07-30 12:08:44 +0900

    Feat: 관심 상품 등록 기능

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -2,7 +2,9 @@ package com.sparta.myselectshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //엔티티 객체가 생성이 되거나 변경이 되었을 때 자동으로 값을 등록
 @SpringBootApplication
 public class MyselectshopApplication {
 
