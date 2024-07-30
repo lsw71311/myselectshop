eb76d01437ffb787f2be4438a013f1129fd6ebaa
lsw71311 <lsw71311@naver.com>
2024-07-30 12:36:09 +0900

    Feat: 관심상품 희망 최저가 설정 기능

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -1,9 +1,11 @@
 package com.sparta.myselectshop.service;
 
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.repository.ProductRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +14,26 @@ import org.springframework.stereotype.Service;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    public static final int MIN_MY_PRICE=100;
 
     public ProductResponseDto createProduct(ProductRequestDto requestDto) {
         Product product = productRepository.save(new Product(requestDto));
+        return new ProductResponseDto(product);
+    }
+
+    @Transactional //더티체킹 -> 변경 감지 후 수정
+    public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+        int myprice = requestDto.getMyprice();
+
+        if(myprice < MIN_MY_PRICE){
+            throw new IllegalArgumentException("유효하지 않은 관심 가격. 최소 "+MIN_MY_PRICE+"원 이상으로 설정하세요.");
+        }
+
+        Product product = productRepository.findById(id).orElseThrow(() ->
+                new NullPointerException(("해당 상품을 찾을 수 없습니다.")));
+
+        product.update(requestDto);
+
         return new ProductResponseDto(product);
     }
 }
